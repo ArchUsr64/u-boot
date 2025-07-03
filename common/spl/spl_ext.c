b66f75d7e1fd69535a534a175099329d992de901
Anshul Dalal <anshuld@ti.com>
2025-07-03 16:34:06 +0530

    no env on secure boot
    Signed-off-by: Anshul Dalal <anshuld@ti.com>

--- common/spl/spl_ext.c
+++ common/spl/spl_ext.c
@@ -93,7 +93,8 @@ int spl_load_image_ext_os(struct spl_image_info *spl_image,
 		return -1;
 	}
 
-	if (!CONFIG_IS_ENABLED(ENV_SUPPORT)) {
+	if (!CONFIG_IS_ENABLED(ENV_SUPPORT) ||
+	    CONFIG_IS_ENABLED(OS_BOOT_SECURE)) {
 		goto defaults;
 	}
 
